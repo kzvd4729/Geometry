f0a4b5730ab5ae307e5dd8043922934e50f32a5e
kzvd4729 <kzvd4729@gmail.com>
2019-11-21 11:29:41 +0600

    Create f1.cpp

---
+++ f1.cpp
@@ -0,0 +1,27 @@
+//..............................................................................
+#define setp(a) cout<<setprecision(a)<<fixed
+const double eps=1e-9,pi=acos(-1.0);
+
+int sgn(double d)
+{
+  if(fabs(d)<eps)return 0;return d<0.0?-1:1;
+}
+struct point
+{
+  double x,y;
+  point(){};point(double _x,double _y):x(_x),y(_y){}
+  point operator+(point p){return point(x+p.x,y+p.y);}
+  point operator-(point p){return point(x-p.x,y-p.y);}
+  point operator*(double d){return point(x*d,y*d);}
+  point operator/(double d){return point(x/d,y/d);}
+  double operator*(point p)
+  {
+    return x*p.x+y*p.y;//0 if vectors are perpendicular;
+  }
+  double operator^(point p)
+  {
+    return x*p.y-y*p.x;//0 if two vectors are same
+    //sigbed area of the parallelogram by two vector
+    //positive if p is ccw to the point
+  }
+};
